7fdd7368086d432084db84e12138c10957899683
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-10-18 02:09:46 -0400

    first commit

---
+++ src/com/shapesnake/Main.java
@@ -0,0 +1,9 @@
+package com.shapesnake; /**
+ * Created by Hasan Y Ahmed on 10/17/17.
+ */
+import com.shapegame.Game;
+public class Main extends Game{
+    public static void main(String[] args){
+        new Main().run();
+    }
+}
